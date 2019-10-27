3600ee260a06f89f26781fe9b950722222787c4a
Guntur Maulana Ibrahim <55784684+gunturmaulanaii@users.noreply.github.com>
2019-10-27 19:01:48 +0700

    Add files via upload

---
+++ PBO1-10118032-Latihan59-DetectiveConan/src/pbo1/pkg10118032/latihan59/detectiveconan/MainCharacter.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118032.latihan59.detectiveconan;
+
+/**
+ *
+ * @author Guntur Maulana Ibrahim
+ * NAMA  : Guntur Maulana Ibrahim
+ * KELAS : IF 1
+ * NIM   : 10118032
+ */
+public class MainCharacter extends Character{
+    
+    private final String speciality;
+    private final String goal;
+
+    public MainCharacter(String speciality, String goal, String name, String from, boolean eyeglasses) {
+        super(name, from, eyeglasses);
+        this.speciality = speciality;
+        this.goal = goal;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+    
+    public void displayCharacter() {
+        System.out.println("-----");
+        System.out.println("Nama : " + name);
+        System.out.println("-----");
+        System.out.println("Dari : " + from);
+        if(eyeglasses) {
+            System.out.println("Kecamata : Iya");
+        } else {
+            System.out.println("Kecamata : Tidak");
+        }
+        System.out.println("Specialis : " + speciality);
+        System.out.println("Tujuan : " + goal);
+        System.out.println();
+    }
+    
+}
---
+++ PBO1-10118032-Latihan59-DetectiveConan/src/pbo1/pkg10118032/latihan59/detectiveconan/SupportCharacter.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118032.latihan59.detectiveconan;
+
+/**
+ *
+ * @author Guntur Maulana Ibrahim
+ * NAMA  : Guntur Maulana Ibrahim
+ * KELAS : IF 1
+ * NIM   : 10118032
+ */
+public class SupportCharacter extends Character{
+    
+    private final String related;
+
+    public SupportCharacter(String related, String name, String from, boolean eyeglasses) {
+        super(name, from, eyeglasses);
+        this.related = related;
+    }
+    
+    public String getRelated() {
+        return related;
+    }
+
+    public void displayCharacter() {
+        System.out.println("-----");
+        System.out.println("Nama : " + name);
+        System.out.println("-----");
+        System.out.println("Dari : " + from);
+        if(eyeglasses) {
+            System.out.println("Kecamata : Iya");
+        } else {
+            System.out.println("Kecamata : Tidak");
+        }
+        System.out.println("Hubungan dengan tokoh utama : " + related);
+        System.out.println();
+    }
+    
+}
